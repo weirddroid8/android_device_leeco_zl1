ea9a25e45a26b3548cc2d329d93fee5beecbdef4
mosimchah <mosimchah@gmail.com>
2020-04-26 18:34:10 -0400

    zl1: import biometrics from aosp 10
    Change-Id: I65e4a52b32531cf450dda5fcc53f4ae772a06027

--- biometrics/service.cpp
+++ biometrics/service.cpp
@@ -14,21 +14,14 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "biometrics.fingerprint@2.0-service.leeco_zl1"
-
-#include <binder/ProcessState.h>
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service"
 
 #include <android/log.h>
 #include <hidl/HidlSupport.h>
 #include <hidl/HidlTransportSupport.h>
 #include <android/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
 #include <android/hardware/biometrics/fingerprint/2.1/types.h>
-
 #include "BiometricsFingerprint.h"
-#include <cutils/properties.h>
-#include <binder/ProcessState.h>
-#include <errno.h>
-#include <unistd.h>
 
 using android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint;
 using android::hardware::biometrics::fingerprint::V2_1::implementation::BiometricsFingerprint;
@@ -36,38 +29,10 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 using android::sp;
 
-bool is_goodix = false;
-
-static constexpr char kGoodixFpDev[] = "/dev/goodix_fp";
-
 int main() {
-    char vend[PROPERTY_VALUE_MAX];
-    property_get("ro.hardware.fingerprint", vend, "none");
-
-    if (!strcmp(vend, "none")) {
-        ALOGE("ro.hardware.fingerprint not set! Killing " LOG_TAG " binder service!");
-        return 1;
-    } else if (!strcmp(vend, "goodix")) {
-        ALOGI("is_goodix = true");
-        is_goodix = true;
-    }
-
-    ALOGI("Start biometrics");
     android::sp<IBiometricsFingerprint> bio = BiometricsFingerprint::getInstance();
 
-    if (is_goodix) {
-        if (access(kGoodixFpDev, F_OK) != 0) {
-            ALOGE("Cannot access %s (%s)", kGoodixFpDev, strerror(errno));
-            return 1;
-        }
-
-        // the conventional HAL might start binder services
-        android::ProcessState::initWithDriver("/dev/binder");
-        android::ProcessState::self()->startThreadPool();
-    }
-
-    /* process Binder transaction as a single-threaded program. */
-    configureRpcThreadpool(1, true /* callerWillJoin */);
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
 
     if (bio != nullptr) {
         if (::android::OK != bio->registerAsService()) {
